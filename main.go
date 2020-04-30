43d9da4ba6a72284e76026fb6cae1c986b04f5b3
Dmitry <dimitriy@captureproof.com>
2020-04-30 04:22:32 +0300

    fix

--- main.go
+++ main.go
@@ -1,45 +1,10 @@
-package main
+package exel_parser
 
 import (
 	"fmt"
-	"github.com/360EntSecGroup-Skylar/excelize"
+	"exel_parser/pkg/excelize"
 )
 
-type XLSXReader struct {
-	filename string
-}
-
-func (xlsxReader *XLSXReader) GetCellValue(sheet string, axis string) (string, error) {
-	f := xlsxReader.openFile()
-
-	cell, err := f.GetCellValue(sheet, axis)
-	if err != nil {
-		return "", err
-	}
-	return cell, err
-}
-
-func (xlsxReader *XLSXReader) openFile() *excelize.File {
-	if len(xlsxReader.filename) == 0 {
-		return nil
-	}
-
-	f, err := excelize.OpenFile(xlsxReader.filename)
-	if err != nil {
-		fmt.Println(err)
-		return nil
-	}
-
-	return f
-}
-
-func (xlsxReader *XLSXReader) GetRows(sheet string) ([][]string, error) {
-	f := xlsxReader.openFile()
-
-	rows, err := f.GetRows(sheet)
-
-	return rows, err
-}
 
 func main() {
 	xlsxReader := XLSXReader{"example.xlsx"}
---
+++ pkg/excelize/excelize.go
@@ -0,0 +1,39 @@
+package excelize
+
+import "github.com/360EntSecGroup-Skylar/excelize"
+
+type XLSXReader struct {
+	filename string
+}
+
+func (xlsxReader *XLSXReader) GetCellValue(sheet string, axis string) (string, error) {
+	f := xlsxReader.openFile()
+
+	cell, err := f.GetCellValue(sheet, axis)
+	if err != nil {
+		return "", err
+	}
+	return cell, err
+}
+
+func (xlsxReader *XLSXReader) openFile() *excelize.File {
+	if len(xlsxReader.filename) == 0 {
+		return nil
+	}
+
+	f, err := excelize.OpenFile(xlsxReader.filename)
+	if err != nil {
+		fmt.Println(err)
+		return nil
+	}
+
+	return f
+}
+
+func (xlsxReader *XLSXReader) GetRows(sheet string) ([][]string, error) {
+	f := xlsxReader.openFile()
+
+	rows, err := f.GetRows(sheet)
+
+	return rows, err
+}
